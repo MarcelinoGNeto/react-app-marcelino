3b6507bceaa6951a7f0c2eb1f661a13f469e0ed3
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-10-19 21:01:43 -0300

    feat: basic routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './pages/login';
+
 
 function App() {
   return (
+
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
-
 export default App;
