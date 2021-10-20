1822ef8273a6f79e4f0147dcd9bdc879b1043c57
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-10-20 08:34:51 -0300

    feat: finalizing routes in this first moment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/login';
+import Routes from './pages/routes';
 
 
 function App() {
   return (
 
     <div className="App">
-      <Login />
+      <Routes />
     </div>
   );
 }
---
+++ src/pages/routes/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+const Routes = () => {
+    return (
+        
+        <div>
+
+        </div>
+    );
+}
+
+export default Routes;
